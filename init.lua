c0b786aa7d76cbff60e299c2058b5e2dbea829e6
P Paul Jonathan <ppauljonathan@gmail.com>
2025-06-07 16:53:36 +0530

    use everforest theme

--- init.lua
+++ init.lua
@@ -948,14 +948,15 @@ require('lazy').setup({
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'ellisonleao/gruvbox.nvim', -- gruvbox
+    -- 'ellisonleao/gruvbox.nvim', -- gruvbox
+    'sainnhe/everforest',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     config = function()
       ---@diagnostic disable-next-line: missing-fields
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'gruvbox'
+      vim.cmd.colorscheme 'everforest'
     end,
   },
 
