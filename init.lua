d0beeaeca40868dc2e9d1fae0d9b175552cf979f
= <=>
2025-07-07 12:26:53 +0530

    add multi themes

--- init.lua
+++ init.lua
@@ -948,22 +948,16 @@ require('lazy').setup({
     },
   },
 
-  { -- You can easily change to a different colorscheme.
-    -- Change the name of the colorscheme plugin below, and then
-    -- change the command in the config to whatever the name of that colorscheme is.
-    --
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    -- 'projekt0n/github-nvim-theme', --  github
-    -- 'neanias/everforest-nvim',
-    'ellisonleao/gruvbox.nvim',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
+  { 'projekt0n/github-nvim-theme' }, --  github
+  { 'neanias/everforest-nvim' },
+  { 'ellisonleao/gruvbox.nvim'},
+  {
+    'shaunsingh/nord.nvim',
+    lazy = false,
+    priority = 1000,
     config = function()
-      ---@diagnostic disable-next-line: missing-fields
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'gruvbox'
-    end,
+      vim.cmd('colorscheme nord')
+    end
   },
 
   -- Highlight todo, notes, etc in comments
